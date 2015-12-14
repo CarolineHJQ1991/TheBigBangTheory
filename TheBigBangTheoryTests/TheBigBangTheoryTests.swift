13d6e607dab0191d0913775e2ae35e8fd8e77d7e
caroline <caroline.hjq@gmail.com>
2015-12-14 14:21:07 +0800

    Initial Commit

---
+++ TheBigBangTheoryTests/TheBigBangTheoryTests.swift
@@ -0,0 +1,36 @@
+//
+//  TheBigBangTheoryTests.swift
+//  TheBigBangTheoryTests
+//
+//  Created by Caroline.H on 12/14/15.
+//  Copyright © 2015 Caroline.H. All rights reserved.
+//
+
+import XCTest
+@testable import TheBigBangTheory
+
+class TheBigBangTheoryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
